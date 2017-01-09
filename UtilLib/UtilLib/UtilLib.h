d00625a72c85f85f16b8d6ac37e56fc130afa45e
xiaoL0204 <550712787@qq.com>
2017-01-09 16:40:52 +0800

    .a file package into framework

---
+++ UtilLib/UtilLib/UtilLib.h
@@ -0,0 +1,19 @@
+//
+//  UtilLib.h
+//  UtilLib
+//
+//  Created by xiaoL on 17/1/9.
+//  Copyright © 2017年 xiaolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilLib.
+FOUNDATION_EXPORT double UtilLibVersionNumber;
+
+//! Project version string for UtilLib.
+FOUNDATION_EXPORT const unsigned char UtilLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilLib/PublicHeader.h>
+
+
---
+++ WorkProj/WorkProj-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  WorkProj-Bridging-Header.h
+//  WorkProj
+//
+//  Created by xiaoL on 17/1/9.
+//  Copyright © 2017年 xiaolin. All rights reserved.
+//
+
+#ifndef WorkProj_Bridging_Header_h
+#define WorkProj_Bridging_Header_h
+
+
+#import <UtilLib/JPUSHService.h>
+
+#endif /* WorkProj_Bridging_Header_h */
